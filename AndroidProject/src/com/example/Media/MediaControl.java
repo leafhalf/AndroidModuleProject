4631da04fa3e73c6057906b3c63a376bf7413916
Administrator <Administrator@192.168.0.103>
2014-04-21 00:26:46 +0800

    new playback module

--- AndroidProject/src/com/example/Media/MediaControl.java
+++ AndroidProject/src/com/example/Media/MediaControl.java
@@ -1,17 +1,51 @@
 package com.example.Media;
 
+import java.io.IOException;
+import java.util.Map;
+
 import android.media.MediaPlayer;
+import android.util.Log;
+import android.media.MediaPlayer.OnPreparedListener;;
 
 public class MediaControl extends MediaPlayer {
-    public void start(){
-    	MediaPlayer mediaPlayer=new MediaPlayer();
-    	mediaPlayer.setOnBufferingUpdateListener(new OnBufferingUpdateListener() {
-			
-			@Override
-			public void onBufferingUpdate(MediaPlayer mp, int percent) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
+	
+	private String TAG="MediaControl";
+	
+    
+    private static  MediaControl mediaControl;
+    private static MediaPlayer mediaPlayer;
+    
+    public static MediaControl getInstance(){
+    	if(mediaControl!=null)
+    		return mediaControl;
+    	return null;
     }
+    
+    public static void initMediaPlay(){
+    	if(mediaControl==null)
+    		mediaControl=new MediaControl();
+    	if(mediaPlayer==null)
+    		mediaPlayer=new MediaPlayer();
+    }
+    
+    public static void Play(){
+    	
+    }
+    
+    public static void Pause(){
+    	
+    }
+    
+    public static void Next(){
+    	
+    }
+    
+    public static void Previous(){
+    	
+    }
+    
+    public static void setLoopMode(int LOOP_MODE){
+    	
+    }
+    
 }
